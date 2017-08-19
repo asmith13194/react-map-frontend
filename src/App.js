3c7057140e61eb27451c9fc65127c6381df920f2
Adam Smith <asmith13194@gmail.com>
2017-08-19 12:33:50 -0600

    connect front and back

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+      places: [],
+    }
+
+  async componentDidMount() {
+    const response = await fetch('http://localhost:8000/places')
+    const json = await response.json()
+    this.setState({places: json})
+    console.log(this.state.places);
+  }
+
   render() {
     return (
       <div className="App">
