8cd5c59161c9b88a29764674ee5b533e07583522
Shannon Rivers <shannon.rivers@gmail.com>
2017-08-20 15:12:45 -0600

    use react-app-env to enable process.env variables

--- src/components/Map.js
+++ src/components/Map.js
@@ -7,24 +7,24 @@ class Map extends Component{
    }
    render(){
      const Map = ReactMapboxGl({
-       accessToken: 'pk.eyJ1Ijoic2VuYmVuaXRvIiwiYSI6ImNqNmRxM29xcjIzb3gycW9sd2p2NnZla2EifQ.WbU7RsVoRCtJyLQM63c8mw'
+       accessToken: process.env.REACT_APP_ACCESS_TOKEN
      });
 
-      return(
-        <Map
-          style="mapbox://styles/mapbox/streets-v9"
-          containerStyle={{
-            height: "100vh",
-            width: "100vw"
-          }}>
-            <Layer
-              type="symbol"
-              id="marker"
-              layout={{ "icon-image": "marker-15" }}>
-              <Feature coordinates={[-0.481747846041145, 51.3233379650232]}/>
-            </Layer>
-        </Map>
-      )
+     return(
+       <Map
+         style="mapbox://styles/mapbox/streets-v9"
+         containerStyle={{
+           height: "100vh",
+           width: "100vw"
+         }}>
+           <Layer
+             type="symbol"
+             id="marker"
+             layout={{ "icon-image": "marker-15" }}>
+             <Feature coordinates={[-0.481747846041145, 51.3233379650232]}/>
+           </Layer>
+       </Map>
+     )
    }
 }
 
